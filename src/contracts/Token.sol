af5ee7437193ff75c5dbfceff6b26e5a15515f00
Latricia <tricianickelberry@gmail.com>
2021-08-23 20:04:50 -0700

    dbank dapp.

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,28 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("Decentralized Bank Currency", "DCB") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dbank) public returns(bool) {
+    require(msg.sender == minter,'Error, Only owner can change pass minter role');
+    minter=dbank;
+
+    emit MinterChanged(msg.sender, dbank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter,'Error, msg.sender does not have minter role');
 		_mint(account, amount);
 	}
 }
