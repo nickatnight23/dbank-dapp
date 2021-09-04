96c80c9c968f6843e6fe5359551db4b158bb3b96
Latricia <tricianickelberry@gmail.com>
2021-09-04 14:41:46 -0700

    calculating interest

--- src/contracts/dBank.sol
+++ src/contracts/dBank.sol
@@ -48,6 +48,8 @@ contract dBank {
 
     //calc interest per second
     //calc accrued interest
+    uint interestPerSecond = 31668017 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
 
     //send eth to user
     msg.sender.transfer(etherBalanceOf[msg.sender]);
