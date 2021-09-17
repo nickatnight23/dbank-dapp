87de2049b5dfdf1f63455cf9636597c3cd2adf99
Latricia <tricianickelberry@gmail.com>
2021-09-17 13:02:50 -0700

    updated constructor

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -10,7 +10,7 @@ contract Token is ERC20 {
   //add minter changed event
   event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Decentralized Bank Currency", "DBC") {
+  constructor() public payable ERC20("Decentralized Currency", "DBC") {
     //asign initial minter
     minter = msg.sender;
   }
